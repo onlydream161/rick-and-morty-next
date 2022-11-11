e814f89418f124a877785d1ea6fc9072f5278840
Ruslan <kozin_r@webant.ru>
2022-11-11 19:43:22 +0300

    fix jest utils

--- jest/utils.tsx
+++ jest/utils.tsx
@@ -4,12 +4,17 @@ import { FCWithChildren } from '@/shared/@types'
 import { withProviders } from '@/app/providers'
 import { server } from '@/app/mocks-server/server'
 
+import('@/app/mocks-server/server').then(mod => mod.server.listen())
+
 const TestWrapper: FCWithChildren = ({ children }) => {
   return <>{children}</>
 }
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: TestWrapper, ...options })
+
+const customRenderWithProviders = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
   render(ui, { wrapper: withProviders(TestWrapper), ...options })
 
 export * from '@testing-library/react'
-export { customRender as render, server }
+export { customRender as render, customRenderWithProviders as renderWithProviders, server }
